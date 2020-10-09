030c574ef7c5c5e4416dac4d2f8f7628e769043e
Benjamin Gobeil <bkgobeil@gmail.com>
2020-10-09 11:10:39 -0400

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,135 @@
+module logger.App
+
+open System
+open System.IO
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Cors.Infrastructure
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+open FSharp.Control.Tasks.V2.ContextInsensitive
+
+// ---------------------------------
+// Models
+// ---------------------------------
+
+type Message =
+    {
+        Text : string
+    }
+
+// ---------------------------------
+// Views
+// ---------------------------------
+
+module Views =
+    open GiraffeViewEngine
+
+    let layout (content: XmlNode list) =
+        html [] [
+            head [] [
+                title []  [ encodedText "logger" ]
+                link [ _rel  "stylesheet"
+                       _type "text/css"
+                       _href "/main.css" ]
+            ]
+            body [] content
+        ]
+
+    let partial () =
+        h1 [] [ encodedText "logger" ]
+
+    let index (model : Message) =
+        [
+            partial()
+            p [] [ encodedText model.Text ]
+        ] |> layout
+
+// ---------------------------------
+// Web app
+// ---------------------------------
+
+let indexHandler (log : string) =
+    printfn "%s: %s" (DateTime.Now.ToString()) log
+    let model     = { Text = log }
+    let view      = Views.index model
+    htmlView view
+
+let logHandler: HttpHandler =
+    fun next ctx ->
+        task {
+            use streamReader = new StreamReader(ctx.Request.Body)
+            let! string = streamReader.ReadToEndAsync()
+            do printfn "%s: %s" (DateTime.Now.ToString()) string
+            return! text "OK" next ctx
+        }
+            
+let webApp =
+    choose [
+        GET >=>
+            choose [
+                route "/" >=> indexHandler "world"
+                
+            ]
+        POST >=> 
+            choose [
+                route "/log" >=> logHandler
+            ]
+        setStatusCode 404 >=> text "Not Found" ]
+
+// ---------------------------------
+// Error handler
+// ---------------------------------
+
+let errorHandler (ex : Exception) (logger : ILogger) =
+    logger.LogError(ex, "An unhandled exception has occurred while executing the request.")
+    clearResponse >=> setStatusCode 500 >=> text ex.Message
+
+// ---------------------------------
+// Config and Main
+// ---------------------------------
+
+let configureCors (builder : CorsPolicyBuilder) =
+    builder.AllowAnyMethod()
+           .AllowAnyHeader()
+           .AllowAnyOrigin()
+           |> ignore
+
+let configureApp (app : IApplicationBuilder) =
+    let env = app.ApplicationServices.GetService<IWebHostEnvironment>()
+    (match env.EnvironmentName with
+    | "Development" -> app.UseDeveloperExceptionPage()
+    | _ -> app.UseGiraffeErrorHandler(errorHandler))
+        .UseCors(configureCors)
+        .UseStaticFiles()
+        .UseGiraffe(webApp)
+
+let configureServices (services : IServiceCollection) =
+    services.AddCors()    |> ignore
+    services.AddGiraffe() |> ignore
+
+let configureLogging (builder : ILoggingBuilder) =
+    builder.AddFilter(fun l -> l.Equals LogLevel.Error)
+           .AddConsole()
+           .AddDebug() |> ignore
+
+[<EntryPoint>]
+let main args =
+    let contentRoot = Directory.GetCurrentDirectory()
+    let webRoot     = Path.Combine(contentRoot, "WebRoot")
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(
+            fun webHostBuilder ->
+                webHostBuilder
+                    .UseContentRoot(contentRoot)
+                    .UseUrls("http://192.168.1.2:5001")
+                    .UseWebRoot(webRoot)
+                    .Configure(Action<IApplicationBuilder> configureApp)
+                    .ConfigureServices(configureServices)
+                    .ConfigureLogging(configureLogging)
+                    |> ignore)
+        .Build()
+        .Run()
+    0
